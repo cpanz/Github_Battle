a228ae4b1d442a8ecade52816349a30edfb7ff4f
Cheng Pan <cproripop@gmail.com>
2016-08-12 13:25:21 -0400

    Add react css transition

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,11 +1,17 @@
 var
-  React = require('react');
+  React = require('react'),
+  ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 var Main = React.createClass({
   render: function() {
     return(
       <div className='main-container'>
-        {this.props.children}
+        <ReactCSSTransitionGroup
+          transitionName='appear'
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}>
+          {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+        </ReactCSSTransitionGroup>
       </div>
     );
   }
